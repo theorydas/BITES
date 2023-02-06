8c73e1b2955c3084a9a3042b6aaf19fcc9b3ba07
KittenPirate <friesbi7porto@gmail.com>
2023-02-06 23:18:09 +0100

    Read the recipe list and keep track of reagents

--- BitesCookBook/BitesCookBook.lua
+++ BitesCookBook/BitesCookBook.lua
@@ -1,11 +1,15 @@
+local recipes
 local CookingSkillRank
+local ingredients
 
--- When the addon is loaded, we want to get the player's cooking skill.
+-- When the addon is loaded, we want to get the player's cooking skill, and create a list of all ingredients.
 function OnEvent(self, event, ...)
     if event == "ADDON_LOADED" and ... == "BitesCookBook" then
         -- Get the player's cooking skill.
         print("Addon has loaded")
         CookingSkillRank = GetSkillLevel("Cooking")
+        -- Create a list of all ingredients.
+        ingredients = GetAllIngredients(recipe)
 
         -- Unregister the event, we do not need it anymore.
         self:UnregisterEvent("ADDON_LOADED");
@@ -15,6 +19,12 @@ function OnEvent(self, event, ...)
         CookingSkillRank = GetSkillLevel("Cooking")
     end
 end
+
+-- Create a frame to listen to events.
+local TemporaryFrame = CreateFrame("Frame")
+TemporaryFrame:RegisterEvent("ADDON_LOADED")
+TemporaryFrame:RegisterEvent("CHAT_MSG_SKILL")
+TemporaryFrame:SetScript("OnEvent", OnEvent)
 
 function GetSkillLevel(SkillName)
     for skillIndex = 1, GetNumSkillLines() do
@@ -29,8 +39,25 @@ function GetSkillLevel(SkillName)
     return 0
 end
 
--- Create a frame to listen to events.
-local TemporaryFrame = CreateFrame("Frame")
-TemporaryFrame:RegisterEvent("ADDON_LOADED")
-TemporaryFrame:RegisterEvent("CHAT_MSG_SKILL")
-TemporaryFrame:SetScript("OnEvent", OnEvent)
+function GetAllIngredients(RecipeList)
+    local ingredients = {}
+    
+    for recipe_key, recipe in pairs(RecipeList) do
+        for ingredient, _ in pairs(recipe["Materials"]) do
+            if ingredients[ingredient] == nil then
+                ingredients[ingredient] = {recipe_key}
+            else
+                table.insert(ingredients[ingredient], recipe_key)
+            end
+        end
+    end
+
+    -- sort each ingredient list based on the recipe range[1]
+    for ingredient, recipe_list in pairs(ingredients) do
+        table.sort(recipe_list, function(a, b)
+            return RecipeList[a]["Range"][1] > RecipeList[b]["Range"][1]
+        end)
+    end
+
+    return ingredients
+end
