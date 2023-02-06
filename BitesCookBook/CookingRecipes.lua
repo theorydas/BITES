a30f06f4b99c75eceb16e4b01c4ac0d1c1d8a8c1
KittenPirate <friesbi7porto@gmail.com>
2023-02-06 23:18:28 +0100

    Add cooking recipe list

---
+++ BitesCookBook/CookingRecipes.lua
@@ -0,0 +1,808 @@
+local recipes = {
+	[2679] = { -- Charred Wolf Meat
+		["Amount"] = 1,
+		["Range"] = {0, 45, 65, 85},
+		["Source"] = {"World"},
+		["Materials"] = {
+			[2672] = 1, --Stringy Wolf Meat
+		},
+	},
+
+	[2680] = { -- Spiced Wolf Meat
+		["Amount"] = 1,
+		["Range"] = {10, 50, 70, 90},
+		["Source"] = {"Trainer"},
+		["Materials"] = {
+			[2672] = 1, --Stringy Wolf Meat
+			[2678] = 1, --Mild Spices
+		},
+	},
+
+	[2681] = { -- Roasted Boar Meat
+		["Amount"] = 1,
+		["Range"] = {0, 45, 65, 85},
+		["Source"] = {"World"},
+		["Materials"] = {
+			[769] = 1, --Chunk of Boar Meat
+		},
+	},
+
+	[2684] = { -- Coyote Steak
+		["Amount"] = 1,
+		["Range"] = {50, 90, 110, 130},
+		["Source"] = {"Trainer"},
+		["Materials"] = {
+			[2673] = 1, --Coyote Meat
+		},
+	},
+
+	[724] = { -- Goretusk Liver Pie
+		["Amount"] = 1,
+		["Range"] = {50, 90, 110, 130},
+		["Source"] = {"World"},
+		["Materials"] = {
+			[723] = 1, --Goretusk Liver
+			[2678] = 1, --Mild Spices
+		},
+	},
+
+	[733] = { -- Westfall Stew
+		["Amount"] = 1,
+		["Range"] = {75, 115, 135, 155},
+		["Source"] = {"World"},
+		["Materials"] = {
+			[729] = 1, --Stringy Vulture Meat
+			[730] = 1, --Murloc Eye
+			[731] = 1, --Goretusk Snout
+		},
+	},
+
+	[2683] = { -- Crab Cake
+		["Amount"] = 1,
+		["Range"] = {75, 115, 135, 155},
+		["Source"] = {"Trainer"},
+		["Materials"] = {
+			[2674] = 1, --Crawler Meat
+			[2678] = 1, --Mild Spices
+		},
+	},
+
+	[2682] = { -- Cooked Crab Claw
+		["Amount"] = 1,
+		["Range"] = {85, 125, 145, 165},
+		["Source"] = {"World"},
+		["Materials"] = {
+			[2675] = 1, --Crawler Claw
+			[2678] = 1, --Mild Spices
+		},
+	},
+
+	[2687] = { -- Dry Pork Ribs
+		["Amount"] = 1,
+		["Range"] = {80, 120, 140, 160},
+		["Source"] = {"Trainer"},
+		["Materials"] = {
+			[2677] = 1, --Boar Ribs
+			[2678] = 1, --Mild Spices
+		},
+	},
+
+	[1082] = { -- Redridge Goulash
+		["Amount"] = 1,
+		["Range"] = {100, 135, 155, 175},
+		["Source"] = {"World"},
+		["Materials"] = {
+			[1081] = 1, --Crisp Spider Meat
+			[1080] = 1, --Tough Condor Meat
+		},
+	},
+
+	[2685] = { -- Succulent Pork Ribs
+		["Amount"] = 1,
+		["Range"] = {110, 130, 150, 170},
+		["Source"] = {"World"},
+		["Materials"] = {
+			[2677] = 2, --Boar Ribs
+			[2692] = 1, --Hot Spices
+		},
+	},
+
+	[1017] = { -- Seasoned Wolf Kabob
+		["Amount"] = 3,
+		["Range"] = {100, 140, 160, 180},
+		["Source"] = {"World"},
+		["Materials"] = {
+			[1015] = 2, --Lean Wolf Flank
+			[2665] = 1, --Stormwind Seasoning Herbs
+		},
+	},
+
+	[2888] = { -- Beer Basted Boar Ribs
+		["Amount"] = 1,
+		["Range"] = {25, 60, 80, 100},
+		["Source"] = {"World"},
+		["Materials"] = {
+			[2886] = 1, --Crag Boar Rib
+			[2894] = 1, --Rhapsody Malt
+		},
+	},
+
+	[3662] = { -- Crocolisk Steak
+		["Amount"] = 1,
+		["Range"] = {80, 120, 140, 160},
+		["Source"] = {"World"},
+		["Materials"] = {
+			[2924] = 1, --Crocolisk Meat
+			[2678] = 1, --Mild Spices
+		},
+	},
+
+	[3220] = { -- Blood Sausage
+		["Amount"] = 2,
+		["Range"] = {60, 100, 120, 140},
+		["Source"] = {"World"},
+		["Materials"] = {
+			[3173] = 1, --Bear Meat
+			[3172] = 1, --Boar Intestines
+			[3174] = 1, --Spider Ichor
+		},
+	},
+
+	[3663] = { -- Murloc Fin Soup
+		["Amount"] = 1,
+		["Range"] = {90, 130, 150, 170},
+		["Source"] = {"World"},
+		["Materials"] = {
+			[1468] = 2, --Murloc Fin
+			[2692] = 1, --Hot Spices
+		},
+	},
+
+	[3664] = { -- Crocolisk Gumbo
+		["Amount"] = 1,
+		["Range"] = {120, 160, 180, 200},
+		["Source"] = {"World"},
+		["Materials"] = {
+			[3667] = 1, --Tender Crocolisk Meat
+			[2692] = 1, --Hot Spices
+		},
+	},
+
+	[3665] = { -- Curiously Tasty Omelet
+		["Amount"] = 1,
+		["Range"] = {130, 170, 190, 210},
+		["Source"] = {"World"},
+		["Materials"] = {
+			[3685] = 1, --Raptor Egg
+			[2692] = 1, --Hot Spices
+		},
+	},
+
+	[3666] = { -- Gooey Spider Cake
+		["Amount"] = 1,
+		["Range"] = {110, 150, 170, 190},
+		["Source"] = {"World"},
+		["Materials"] = {
+			[2251] = 2, --Gooey Spider Leg
+			[2692] = 1, --Hot Spices
+		},
+	},
+
+	[3726] = { -- Big Bear Steak
+		["Amount"] = 1,
+		["Range"] = {110, 150, 170, 190},
+		["Source"] = {"World"},
+		["Materials"] = {
+			[3730] = 1, --Big Bear Meat
+			[2692] = 1, --Hot Spices
+		},
+	},
+
+	[3727] = { -- Hot Lion Chops
+		["Amount"] = 1,
+		["Range"] = {125, 175, 195, 215},
+		["Source"] = {"World"},
+		["Materials"] = {
+			[3731] = 1, --Lion Meat
+			[2692] = 1, --Hot Spices
+		},
+	},
+
+	[3728] = { -- Tasty Lion Steak
+		["Amount"] = 1,
+		["Range"] = {150, 190, 210, 230},
+		["Source"] = {"World"},
+		["Materials"] = {
+			[3731] = 2, --Lion Meat
+			[3713] = 1, --Soothing Spices
+		},
+	},
+
+	[3729] = { -- Soothing Turtle Bisque
+		["Amount"] = 1,
+		["Range"] = {175, 215, 235, 255},
+		["Source"] = {"World"},
+		["Materials"] = {
+			[3712] = 1, --Turtle Meat
+			[3713] = 1, --Soothing Spices
+		},
+	},
+
+	[4457] = { -- Barbecued Buzzard Wing
+		["Amount"] = 1,
+		["Range"] = {175, 215, 235, 255},
+		["Source"] = {"Trainer"},
+		["Materials"] = {
+			[3404] = 1, --Buzzard Wing
+			[2692] = 1, --Hot Spices
+		},
+	},
+
+	[5472] = { -- Kaldorei Spider Kabob
+		["Amount"] = 1,
+		["Range"] = {10, 50, 70, 90},
+		["Source"] = {"World"},
+		["Materials"] = {
+			[5465] = 1, --Small Spider Leg
+		},
+	},
+
+	[5473] = { -- Scorpid Surprise
+		["Amount"] = 1,
+		["Range"] = {20, 60, 80, 100},
+		["Source"] = {"World"},
+		["Materials"] = {
+			[5466] = 1, --Scorpid Stinger
+		},
+	},
+
+	[5474] = { -- Roasted Kodo Meat
+		["Amount"] = 2,
+		["Range"] = {35, 75, 95, 115},
+		["Source"] = {"World"},
+		["Materials"] = {
+			[5467] = 1, --Kodo Meat
+			[2678] = 1, --Mild Spices
+		},
+	},
+
+	[5476] = { -- Fillet of Frenzy
+		["Amount"] = 2,
+		["Range"] = {50, 90, 110, 130},
+		["Source"] = {"World"},
+		["Materials"] = {
+			[5468] = 1, --Soft Frenzy Flesh
+			[2678] = 1, --Mild Spices
+		},
+	},
+
+	[5477] = { -- Strider Stew
+		["Amount"] = 2,
+		["Range"] = {50, 90, 110, 130},
+		["Source"] = {"World"},
+		["Materials"] = {
+			[5469] = 1, --Strider Meat
+			[4536] = 1, --Shiny Red Apple
+		},
+	},
+
+	[5478] = { -- Dig Rat Stew
+		["Amount"] = 2,
+		["Range"] = {90, 130, 150, 170},
+		["Source"] = {"World"},
+		["Materials"] = {
+			[5051] = 1, --Dig Rat
+		},
+	},
+
+	[5479] = { -- Crispy Lizard Tail
+		["Amount"] = 2,
+		["Range"] = {100, 140, 160, 180},
+		["Source"] = {"World"},
+		["Materials"] = {
+			[5470] = 1, --Thunder Lizard Tail
+			[2692] = 1, --Hot Spices
+		},
+	},
+
+	[5480] = { -- Lean Venison
+		["Amount"] = 2,
+		["Range"] = {110, 150, 170, 190},
+		["Source"] = {"World"},
+		["Materials"] = {
+			[5471] = 1, --Stag Meat
+			[2678] = 4, --Mild Spices
+		},
+	},
+
+	[5525] = { -- Boiled Clams
+		["Amount"] = 1,
+		["Range"] = {50, 90, 110, 130},
+		["Source"] = {"Trainer"},
+		["Materials"] = {
+			[5503] = 1, --Clam Meat
+			[159] = 1, --Refreshing Spring Water
+		},
+	},
+
+	[5527] = { -- Goblin Deviled Clams
+		["Amount"] = 1,
+		["Range"] = {125, 165, 185, 205},
+		["Source"] = {"Trainer"},
+		["Materials"] = {
+			[5504] = 1, --Tangy Clam Meat
+			[2692] = 1, --Hot Spices
+		},
+	},
+
+	[5526] = { -- Clam Chowder
+		["Amount"] = 1,
+		["Range"] = {90, 130, 150, 170},
+		["Source"] = {"World"},
+		["Materials"] = {
+			[5503] = 1, --Clam Meat
+			[1179] = 1, --Ice Cold Milk
+			[2678] = 1, --Mild Spices
+		},
+	},
+
+	[6038] = { -- Giant Clam Scorcho
+		["Amount"] = 1,
+		["Range"] = {175, 215, 235, 255},
+		["Source"] = {"World"},
+		["Materials"] = {
+			[4655] = 1, --Giant Clam Meat
+			[2692] = 1, --Hot Spices
+		},
+	},
+
+	[6290] = { -- Brilliant Smallfish
+		["Amount"] = 1,
+		["Range"] = {1, 45, 65, 85},
+		["Source"] = {"World"},
+		["Materials"] = {
+			[6291] = 1, --Raw Brilliant Smallfish
+		},
+	},
+
+	[787] = { -- Slitherskin Mackerel
+		["Amount"] = 1,
+		["Range"] = {1, 45, 65, 85},
+		["Source"] = {"World"},
+		["Materials"] = {
+			[6303] = 1, --Raw Slitherskin Mackerel
+		},
+	},
+
+	[4592] = { -- Longjaw Mud Snapper
+		["Amount"] = 1,
+		["Range"] = {50, 90, 110, 130},
+		["Source"] = {"World"},
+		["Materials"] = {
+			[6289] = 1, --Raw Longjaw Mud Snapper
+		},
+	},
+
+	[6316] = { -- Loch Frenzy Delight
+		["Amount"] = 1,
+		["Range"] = {50, 90, 110, 130},
+		["Source"] = {"World"},
+		["Materials"] = {
+			[6317] = 1, --Raw Loch Frenzy
+			[2678] = 1, --Mild Spices
+		},
+	},
+
+	[4593] = { -- Bristle Whisker Catfish
+		["Amount"] = 1,
+		["Range"] = {100, 140, 160, 180},
+		["Source"] = {"World"},
+		["Materials"] = {
+			[6308] = 1, --Raw Bristle Whisker Catfish
+		},
+	},
+
+	[5095] = { -- Rainbow Fin Albacore
+		["Amount"] = 1,
+		["Range"] = {50, 90, 110, 130},
+		["Source"] = {"World"},
+		["Materials"] = {
+			[6361] = 1, --Raw Rainbow Fin Albacore
+		},
+	},
+
+	[4594] = { -- Rockscale Cod
+		["Amount"] = 1,
+		["Range"] = {175, 190, 210, 230},
+		["Source"] = {"World"},
+		["Materials"] = {
+			[6362] = 1, --Raw Rockscale Cod
+		},
+	},
+
+	[6657] = { -- Savory Deviate Delight
+		["Amount"] = 1,
+		["Range"] = {85, 125, 145, 165},
+		["Source"] = {"World"},
+		["Materials"] = {
+			[6522] = 1, --Deviate Fish
+			[2678] = 1, --Mild Spices
+		},
+	},
+
+	[6888] = { -- Herb Baked Egg
+		["Amount"] = 1,
+		["Range"] = {1, 45, 65, 85},
+		["Source"] = {"World"},
+		["Materials"] = {
+			[6889] = 1, --Small Egg
+			[2678] = 1, --Mild Spices
+		},
+	},
+
+	[6890] = { -- Smoked Bear Meat
+		["Amount"] = 1,
+		["Range"] = {40, 80, 100, 120},
+		["Source"] = {"World"},
+		["Materials"] = {
+			[3173] = 1, --Bear Meat
+		},
+	},
+
+	[7676] = { -- Thistle Tea
+		["Amount"] = 1,
+		["Range"] = {60, 100, 120, 140},
+		["Source"] = {"World"},
+		["Materials"] = {
+			[2452] = 1, --Swiftthistle
+			[159] = 1, --Refreshing Spring Water
+		},
+	},
+
+	[10841] = { -- Goldthorn Tea
+		["Amount"] = 4,
+		["Range"] = {175, 215, 235, 255},
+		["Source"] = {"World"},
+		["Materials"] = {
+			[3821] = 1, --Goldthorn
+			[159] = 1, --Refreshing Spring Water
+		},
+	},
+
+	[12209] = { -- Lean Wolf Steak
+		["Amount"] = 1,
+		["Range"] = {125, 165, 185, 205},
+		["Source"] = {"World"},
+		["Materials"] = {
+			[1015] = 1, --Lean Wolf Flank
+			[2678] = 1, --Mild Spices
+		},
+	},
+
+	[12210] = { -- Roast Raptor
+		["Amount"] = 1,
+		["Range"] = {175, 215, 235, 255},
+		["Source"] = {"World"},
+		["Materials"] = {
+			[12184] = 1, --Raptor Flesh
+			[2692] = 1, --Hot Spices
+		},
+	},
+
+	[13851] = { -- Hot Wolf Ribs
+		["Amount"] = 1,
+		["Range"] = {175, 215, 235, 255},
+		["Source"] = {"World"},
+		["Materials"] = {
+			[12203] = 1, --Red Wolf Meat
+			[2692] = 1, --Hot Spices
+		},
+	},
+
+	[12212] = { -- Jungle Stew
+		["Amount"] = 2,
+		["Range"] = {175, 215, 235, 255},
+		["Source"] = {"World"},
+		["Materials"] = {
+			[12202] = 1, --Tiger Meat
+			[159] = 1, --Refreshing Spring Water
+			[4536] = 2, --Shiny Red Apple
+		},
+	},
+
+	[12213] = { -- Carrion Surprise
+		["Amount"] = 1,
+		["Range"] = {175, 215, 235, 255},
+		["Source"] = {"World"},
+		["Materials"] = {
+			[12037] = 1, --Mystery Meat
+			[2692] = 1, --Hot Spices
+		},
+	},
+
+	[12214] = { -- Mystery Stew
+		["Amount"] = 1,
+		["Range"] = {175, 215, 235, 255},
+		["Source"] = {"World"},
+		["Materials"] = {
+			[12037] = 1, --Mystery Meat
+			[2596] = 1, --Skin of Dwarven Stout
+		},
+	},
+
+	[12217] = { -- Dragonbreath Chili
+		["Amount"] = 1,
+		["Range"] = {200, 240, 260, 280},
+		["Source"] = {"World"},
+		["Materials"] = {
+			[12037] = 1, --Mystery Meat
+			[4402] = 1, --Small Flame Sac
+			[2692] = 1, --Hot Spices
+		},
+	},
+
+	[12215] = { -- Heavy Kodo Stew
+		["Amount"] = 2,
+		["Range"] = {200, 240, 260, 280},
+		["Source"] = {"World"},
+		["Materials"] = {
+			[12204] = 2, --Heavy Kodo Meat
+			[3713] = 1, --Soothing Spices
+			[159] = 1, --Refreshing Spring Water
+		},
+	},
+
+	[12216] = { -- Spiced Chili Crab
+		["Amount"] = 1,
+		["Range"] = {225, 265, 285, 305},
+		["Source"] = {"World"},
+		["Materials"] = {
+			[12206] = 1, --Tender Crab Meat
+			[2692] = 2, --Hot Spices
+		},
+	},
+
+	[12218] = { -- Monster Omelet
+		["Amount"] = 1,
+		["Range"] = {225, 265, 285, 305},
+		["Source"] = {"World"},
+		["Materials"] = {
+			[12207] = 1, --Giant Egg
+			[3713] = 2, --Soothing Spices
+		},
+	},
+
+	[12224] = { -- Crispy Bat Wing
+		["Amount"] = 1,
+		["Range"] = {1, 45, 65, 85},
+		["Source"] = {"World"},
+		["Materials"] = {
+			[12223] = 1, --Meaty Bat Wing
+			[2678] = 1, --Mild Spices
+		},
+	},
+
+	[6887] = { -- Spotted Yellowtail
+		["Amount"] = 1,
+		["Range"] = {225, 265, 285, 305},
+		["Source"] = {"World"},
+		["Materials"] = {
+			[4603] = 1, --Raw Spotted Yellowtail
+		},
+	},
+
+	[13927] = { -- Cooked Glossy Mightfish
+		["Amount"] = 1,
+		["Range"] = {225, 265, 285, 305},
+		["Source"] = {"World"},
+		["Materials"] = {
+			[13754] = 1, --Raw Glossy Mightfish
+			[3713] = 1, --Soothing Spices
+		},
+	},
+
+	[13928] = { -- Grilled Squid
+		["Amount"] = 1,
+		["Range"] = {240, 280, 300, 320},
+		["Source"] = {"World"},
+		["Materials"] = {
+			[13755] = 1, --Winter Squid
+			[3713] = 1, --Soothing Spices
+		},
+	},
+
+	[13930] = { -- Filet of Redgill
+		["Amount"] = 1,
+		["Range"] = {225, 265, 285, 305},
+		["Source"] = {"World"},
+		["Materials"] = {
+			[13758] = 1, --Raw Redgill
+		},
+	},
+
+	[13929] = { -- Hot Smoked Bass
+		["Amount"] = 1,
+		["Range"] = {240, 280, 300, 320},
+		["Source"] = {"World"},
+		["Materials"] = {
+			[13756] = 1, --Raw Summer Bass
+			[2692] = 2, --Hot Spices
+		},
+	},
+
+	[13931] = { -- Nightfin Soup
+		["Amount"] = 1,
+		["Range"] = {250, 290, 310, 330},
+		["Source"] = {"World"},
+		["Materials"] = {
+			[13759] = 1, --Raw Nightfin Snapper
+			[159] = 1, --Refreshing Spring Water
+		},
+	},
+
+	[13932] = { -- Poached Sunscale Salmon
+		["Amount"] = 1,
+		["Range"] = {250, 290, 310, 330},
+		["Source"] = {"World"},
+		["Materials"] = {
+			[13760] = 1, --Raw Sunscale Salmon
+		},
+	},
+
+	[13933] = { -- Lobster Stew
+		["Amount"] = 1,
+		["Range"] = {275, 315, 335, 355},
+		["Source"] = {"World"},
+		["Materials"] = {
+			[13888] = 1, --Darkclaw Lobster
+			[159] = 1, --Refreshing Spring Water
+		},
+	},
+
+	[13934] = { -- Mightfish Steak
+		["Amount"] = 1,
+		["Range"] = {275, 315, 335, 355},
+		["Source"] = {"World"},
+		["Materials"] = {
+			[13893] = 1, --Large Raw Mightfish
+			[2692] = 1, --Hot Spices
+			[3713] = 1, --Soothing Spices
+		},
+	},
+
+	[13935] = { -- Baked Salmon
+		["Amount"] = 1,
+		["Range"] = {275, 315, 335, 355},
+		["Source"] = {"World"},
+		["Materials"] = {
+			[13889] = 1, --Raw Whitescale Salmon
+			[3713] = 1, --Soothing Spices
+		},
+	},
+
+	[16766] = { -- Undermine Clam Chowder
+		["Amount"] = 2,
+		["Range"] = {225, 265, 285, 305},
+		["Source"] = {"World"},
+		["Materials"] = {
+			[7974] = 2, --Zesty Clam Meat
+			[2692] = 1, --Hot Spices
+			[1179] = 1, --Ice Cold Milk
+		},
+	},
+
+	[8364] = { -- Mithril Headed Trout
+		["Amount"] = 1,
+		["Range"] = {175, 215, 235, 255},
+		["Source"] = {"World"},
+		["Materials"] = {
+			[8365] = 1, --Raw Mithril Head Trout
+		},
+	},
+
+	[17197] = { -- Gingerbread Cookie
+		["Amount"] = 1,
+		["Range"] = {1, 45, 65, 85},
+		["Source"] = {"World"},
+		["Materials"] = {
+			[6889] = 1, --Small Egg
+			[17194] = 1, --Holiday Spices
+		},
+	},
+
+	[17198] = { -- Egg Nog
+		["Amount"] = 1,
+		["Range"] = {35, 75, 95, 115},
+		["Source"] = {"World"},
+		["Materials"] = {
+			[6889] = 1, --Small Egg
+			[1179] = 1, --Ice Cold Milk
+			[17196] = 1, --Holiday Spirits
+			[17194] = 1, --Holiday Spices
+		},
+	},
+
+	[17222] = { -- Spider Sausage
+		["Amount"] = 1,
+		["Range"] = {200, 240, 260, 280},
+		["Source"] = {"Trainer"},
+		["Materials"] = {
+			[12205] = 2, --White Spider Meat
+		},
+	},
+
+	[18045] = { -- Tender Wolf Steak
+		["Amount"] = 1,
+		["Range"] = {225, 265, 285, 305},
+		["Source"] = {"World"},
+		["Materials"] = {
+			[12208] = 1, --Tender Wolf Meat
+			[3713] = 1, --Soothing Spices
+		},
+	},
+
+	[18254] = { -- Runn Tum Tuber Surprise
+		["Amount"] = 1,
+		["Range"] = {275, 315, 335, 355},
+		["Source"] = {"World"},
+		["Materials"] = {
+			[18255] = 1, --Runn Tum Tuber
+			[3713] = 1, --Soothing Spices
+		},
+	},
+
+	[20074] = { -- Heavy Crocolisk Stew
+		["Amount"] = 1,
+		["Range"] = {150, 160, 180, 200},
+		["Source"] = {"World"},
+		["Materials"] = {
+			[3667] = 2, --Tender Crocolisk Meat
+			[3713] = 1, --Soothing Spices
+		},
+	},
+
+	[20452] = { -- Smoked Desert Dumplings
+		["Amount"] = 1,
+		["Range"] = {285, 325, 345, 365},
+		["Source"] = {"World"},
+		["Materials"] = {
+			[20424] = 1, --Sandworm Meat
+			[3713] = 1, --Soothing Spices
+		},
+	},
+
+	[21023] = { -- Dirge's Kickin' Chimaerok Chops
+		["Amount"] = 5,
+		["Range"] = {300, 325, 345, 365},
+		["Source"] = {"World"},
+		["Materials"] = {
+			[2692] = 1, --Hot Spices
+			[9061] = 1, --Goblin Rocket Fuel
+			[8150] = 1, --Deeprock Salt
+			[21024] = 1, --Chimaerok Tenderloin
+		},
+	},
+
+	[21072] = { -- Smoked Sagefish
+		["Amount"] = 1,
+		["Range"] = {80, 120, 140, 160},
+		["Source"] = {"World"},
+		["Materials"] = {
+			[21071] = 1, --Raw Sagefish
+			[2678] = 1, --Mild Spices
+		},
+	},
+
+	[21217] = { -- Sagefish Delight
+		["Amount"] = 1,
+		["Range"] = {175, 215, 235, 255},
+		["Source"] = {"World"},
+		["Materials"] = {
+			[21153] = 1, --Raw Greater Sagefish
+			[2692] = 1, --Hot Spices
+		},
+	},
+
+}
+
+BitesCookBook_Recipes = recipes
