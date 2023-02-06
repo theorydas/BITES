3e4d258c8450e2dadea01844e5d403a9642f7cff
KittenPirate <friesbi7porto@gmail.com>
2023-02-06 23:27:28 +0100

    Add tooltip detection

--- BitesCookBook/BitesCookBook.lua
+++ BitesCookBook/BitesCookBook.lua
@@ -12,7 +12,7 @@ function OnEvent(self, event, ...)
         ingredients = GetAllIngredients(recipes)
 
         -- Unregister the event, we do not need it anymore.
-        self:UnregisterEvent("ADDON_LOADED");
+        self:UnregisterEvent("ADDON_LOADED")
     
     -- Additionally, we want to update the player's cooking skill when they level up.
     elseif event == "CHAT_MSG_SKILL" then
@@ -61,3 +61,87 @@ function GetAllIngredients(RecipeList)
 
     return ingredients
 end
+
+
+-- =================================================================================================
+-- Handle tooltips
+-- =================================================================================================
+
+
+function GetItem(id)
+    if id == nil then
+        return -- No id, no color.
+    end
+
+    ItemColor = "|r|cffffffff" -- Default color is white. |r is needed to reset the color and prevents leaks.
+    ItemName = C_Item.GetItemNameByID(id)
+
+    -- If the item is not in the localization, make it red.
+    if ItemName == tostring(id) then
+        ItemColor = "|cffff0000"
+        ItemName = "???"
+    end
+
+    return ItemColor, ItemName
+end
+
+function GetRecipeTooltip(id)
+    --- Shows all materials needed for a recipe.
+
+    if recipes[id] ~= nil then
+        local text = "\n"
+        
+        -- Cycle through all materials in a recipe to create the tooltip.
+        text = text .. "Recipe:"
+        for itemID, count in pairs(recipes[id].Materials) do
+            ItemColor, ItemName = GetItem(itemID)
+            
+            text = text .."\n    ".. ItemColor.. count .. " x " .. ItemName
+        end
+
+        return text
+    else
+        return nil
+    end
+end
+
+function GetIngredientTooltip(id)
+    --- Shows all available recipes for that ingredient.
+    if ingredients[id] ~= nil then
+        local text = "\n"
+        -- Cycle through all materials in a recipe to create the tooltip.
+        text = text .. "Ingredient for:"
+
+        for _, RecipeID in ipairs(ingredients[id]) do
+            ItemColor, ItemName = GetItem(RecipeID)
+            
+            text = text .."\n    ".. ItemColor.. ItemName
+        end
+
+        return text
+    else
+        return nil
+    end
+end
+
+function OnTooltipSetItem(tooltip)
+	local itemName, itemLink = tooltip:GetItem()
+	if not itemName then
+        return
+    end
+    
+    local itemID = Item:CreateFromItemLink(itemLink):GetItemID()
+
+
+    RecipeTooltip = GetRecipeTooltip(itemID)
+    if RecipeTooltip ~= nil then
+        tooltip:AddLine(RecipeTooltip)
+    end
+
+    IngredientTooltip = GetIngredientTooltip(itemID)
+    if IngredientTooltip ~= nil then
+        tooltip:AddLine(IngredientTooltip)
+    end
+end
+
+GameTooltip:HookScript("OnTooltipSetItem", OnTooltipSetItem)
